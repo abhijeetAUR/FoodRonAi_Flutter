fbbd95ccc1a670f9e3cc197720c58373e19d5ad3
abhijeetAUR <abhijeetr131@gmail.com>
2019-11-08 00:13:15 +0530

    code for image details page are in one file remaining not total view is scrollable change back button color to white

--- food_ron_ai/lib/ui/ImageDetails.dart
+++ food_ron_ai/lib/ui/ImageDetails.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:food_ron_ai/bloc/ImageDataBloc.dart';
-import 'package:food_ron_ai/customwidgets/CardDetailsView.dart';
 import 'package:food_ron_ai/database/DatabaseHelper.dart';
 import 'package:food_ron_ai/model_class/ImageUploadResponse.dart';
 import 'package:food_ron_ai/stracture/ImageMetaData.dart';
-import '../Global.dart';
 import 'package:food_ron_ai/Global.dart' as Globals;
 
 class ImageDetails extends StatefulWidget {
@@ -21,12 +19,14 @@ class _ImageDetailsState extends State<ImageDetails> {
   final ImageDataBloc _imageDataBloc = ImageDataBloc();
   final ImageUploadResponse imageUploadResponse;
   _ImageDetailsState({@required this.imageUploadResponse});
+  int serve;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    // getMetaDetails();
+    getMetaDetails();
+    serve = 1;
   }
 
   @override
@@ -35,6 +35,172 @@ class _ImageDetailsState extends State<ImageDetails> {
     _imageDataBloc.dispose();
     super.dispose();
   }
+  void getMetaDetails() async {
+    var result =
+        await databaseHelper.getAllMetaRecords(imageUploadResponse.itemMetaId);
+    print(result);
+
+    List<ImageMetaData> updatedListOfImageMetaData = List<ImageMetaData>();
+    for (var item in result) {
+      ImageMetaData imageMetaData = ImageMetaData();
+      imageMetaData = ImageMetaData.fromJson(item);
+      updatedListOfImageMetaData.add(imageMetaData);
+    }
+    _imageDataBloc.passDataToImageList(updatedListOfImageMetaData);
+  }
+
+  Widget imageItemDetailCard(){
+    return StreamBuilder<List<ImageMetaData>>(
+        stream: _imageDataBloc.imageListStream,
+        builder: (BuildContext context,
+            AsyncSnapshot<List<ImageMetaData>> snapshot) {
+          return GridView.builder(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
+            itemCount: snapshot.data.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                child: Hero(
+                  tag: snapshot.data[index].foodname,
+                  child: Material(
+                    child: InkWell(
+                      onTap: () {},
+                      child: GridTile(
+                        child: Container(
+                          child: Column(
+                            children: <Widget>[
+                              new Container(
+                                padding: new EdgeInsets.only(top: 30),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      snapshot.data[index].foodname,
+                                      style: TextStyle(fontSize: 25),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(width: 20.0),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 15, right: 15, top: 10),
+                                child: Slider.adaptive(
+                                  // key: UniqueKey(),
+                                  activeColor: Colors.orangeAccent,
+                                  value: snapshot.data[index].serve
+                                      .truncateToDouble(),
+                                  min: 1,
+                                  max: 10,
+                                  divisions: 9,
+                                  label: "$serve",
+                                  onChanged: (double newServe) {
+                                    setState(() {
+                                      serve = newServe.round();
+                                    });
+                                    Globals.servecount = serve;
+                                    _imageDataBloc.imageServeIncrement
+                                        .add(snapshot.data[index]);
+                                  },
+                                ),
+                              ),
+                              SizedBox(width: 20.0),
+                              Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 10, left: 30),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Text(
+                                        '${Globals.serve} :\t',
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                      Text(
+                                        '${snapshot.data[index].serve}',
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                    ],
+                                  )),
+                              SizedBox(width: 20.0),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Container(
+                                    width:
+                                        MediaQuery.of(context).size.width / 2.2,
+                                    padding: new EdgeInsets.only(left: 30),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        SizedBox(width: 20.0),
+                                        Text(
+                                          '\n${Globals.weight} :\t${snapshot.data[index].weight} \n',
+                                          style: TextStyle(
+                                            fontSize: 20.0,
+                                          ),
+                                        ),
+                                        SizedBox(width: 20.0),
+                                        Text(
+                                          '${Globals.calorie} :\t${snapshot.data[index].cal} \n',
+                                          style: TextStyle(fontSize: 20.0),
+                                        ),
+                                        SizedBox(width: 20.0),
+                                        Text(
+                                          '${Globals.carbohydrates} :\t${snapshot.data[index].card} \n',
+                                          style: TextStyle(fontSize: 20.0),
+                                        ),
+                                        SizedBox(width: 20.0),
+                                        Text(
+                                          '${Globals.carbohydrates} :\t${snapshot.data[index].protin} \n',
+                                          style: TextStyle(fontSize: 20.0),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    width:
+                                        MediaQuery.of(context).size.width / 2,
+                                    padding: new EdgeInsets.only(
+                                        left: 30, right: 10),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        SizedBox(width: 20.0),
+                                        Text(
+                                          '\n${Globals.fat} :\t${snapshot.data[index].fat} \n',
+                                          style: TextStyle(
+                                            fontSize: 20.0,
+                                          ),
+                                        ),
+                                        SizedBox(width: 20.0),
+                                        Text(
+                                          '${Globals.fiber} :\t${snapshot.data[index].fiber} \n',
+                                          style: TextStyle(fontSize: 20.0),
+                                        ),
+                                        SizedBox(width: 20.0),
+                                        Text(
+                                          '${Globals.sugar} :\t${snapshot.data[index].suger} \n',
+                                          style: TextStyle(fontSize: 20.0),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        });
+  }
 
   _saveButtonClicked() async {
     print("Inside save");
@@ -42,35 +208,6 @@ class _ImageDetailsState extends State<ImageDetails> {
     // print(result);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text("$appName"),
-        ),
-        actions: <Widget>[
-          // action button
-          IconButton(
-            icon: Icon(Icons.save),
-            onPressed: () {
-              _saveButtonClicked();
-            },
-          )
-        ],
-      ),
-      body: DetailView(
-        imageUploadResponse: imageUploadResponse,
-      ),
-    );
-  }
-}
-
-class DetailView extends StatelessWidget {
-  final ImageUploadResponse imageUploadResponse;
-  DetailView({@required this.imageUploadResponse});
-
-//REFER THIS
   Widget imageUpdateWidget() {
     return Image.network(
       imageUploadResponse.inf_img_url,
@@ -78,13 +215,9 @@ class DetailView extends StatelessWidget {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-          flex: 2,
-          child: Container(
+  Widget imageViewContainer(){
+    return Container(
+            height: 300,
               child: new SizedBox(
             width: double.infinity,
             child: Card(
@@ -97,17 +230,36 @@ class DetailView extends StatelessWidget {
               elevation: 5,
               margin: EdgeInsets.all(10),
             ),
-          )),
+          ),);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Image Detail",style: TextStyle(color: Colors.white),),
         ),
-        Expanded(
-          flex: 2,
-          child: Container(
-            child: CardDetailsView(
-              imageUploadResponse: imageUploadResponse,
-            ),
-          ),
-        )
-      ],
+        actions: <Widget>[
+          // action button
+          IconButton(
+            color: Colors.white,
+            icon: Icon(Icons.save),
+            onPressed: () {
+              _saveButtonClicked();
+            },
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+        imageViewContainer(),
+         Expanded(
+           flex: 3,
+           child: imageItemDetailCard(),
+         )
+        ],
+      ), 
     );
   }
 }
