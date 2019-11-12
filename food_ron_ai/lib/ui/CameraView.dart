1b2e2585596688e5e3b2adc120c862bf2f56bff5
abhijeetAUR <abhijeetr131@gmail.com>
2019-11-12 18:03:23 +0530

    square camera view

--- food_ron_ai/lib/ui/CameraView.dart
+++ food_ron_ai/lib/ui/CameraView.dart
@@ -4,7 +4,6 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
-
 class TakePictureScreen extends StatefulWidget {
   final CameraDescription camera;
 
@@ -18,36 +17,78 @@ class TakePictureScreen extends StatefulWidget {
 }
 
 class TakePictureScreenState extends State<TakePictureScreen> {
-  // Add two variables to the state class to store the CameraController and
-  // the Future.
   CameraController _controller;
   Future<void> _initializeControllerFuture;
 
   @override
   void initState() {
     super.initState();
-    // To display the current output from the camera,
-    // create a CameraController.
+    
     _controller = CameraController(
-      // Get a specific camera from the list of available cameras.
+      
       widget.camera,
-      // Define the resolution to use.
-      ResolutionPreset.medium,
+      
+      ResolutionPreset.veryHigh,
     );
 
-    // Next, initialize the controller. This returns a Future.
     _initializeControllerFuture = _controller.initialize();
   }
 
   @override
   void dispose() {
-    // Dispose of the controller when the widget is disposed.
+    
     _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // Fill this out in the next steps.
+    return Scaffold(
+      appBar: AppBar(title: Text('Take a picture')),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+           
+            return AspectRatio(
+            aspectRatio: 1,
+            child: ClipRect(
+              child: Transform.scale(
+                scale: 1 / _controller.value.aspectRatio,
+                child: Center(
+                  child: AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: CameraPreview(_controller),
+                  ),
+                ),
+              ),
+            ),
+          );
+          } else {
+        
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        
+        onPressed: () async {
+          
+          try {
+            
+            await _initializeControllerFuture;
+            final path = join(
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+            await _controller.takePicture(path);
+            
+          } catch (e) {
+            print(e);
+          }
+        },
+      ),
+    );
   }
 }
